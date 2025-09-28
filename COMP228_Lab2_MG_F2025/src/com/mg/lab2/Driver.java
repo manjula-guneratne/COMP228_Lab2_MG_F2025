5700e208537add151a1f01b0159e4580b65df5df
Manjula Guneratne <mguneratne3@gmail.com>
2025-09-27 20:27:34 -0400

    Complete

--- COMP228_Lab2_MG_F2025/src/com/mg/lab2/Driver.java
+++ COMP228_Lab2_MG_F2025/src/com/mg/lab2/Driver.java
@@ -1,5 +1,39 @@
 package com.mg.lab2;
+import java.util.Scanner;
+import java.math.BigDecimal;
 
 public class Driver {
+	
+	Scanner scan = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		
+		Driver driver = new Driver(); // Create instance to access non-static scanner
+		
+		System.out.println("Enter 1 value: ");
+		double v1 = driver.scan.nextDouble();
+		
+		System.out.println("Enter 2 value: ");
+		double v2 = driver.scan.nextDouble();
+		
+		System.out.println("Enter 3 value: ");
+		int v3 = driver.scan.nextInt();
+		
+		System.out.println("Enter 4 value: ");
+		int v4 = driver.scan.nextInt();
+		
+		//Method overloaded
+		Interest i1 = new Interest(v1,v2,v3);
+		Interest i2 = new Interest(v1,v2,v3,v4);
+		
+		driver.scan.close();
+	/*
+	 * 5 Objects to be created!
+	 * 
+	 * User input into all 5 Objects
+	 * 
+	 * stopped 45mins left
+	 */
+	}
 
 }
--- COMP228_Lab2_MG_F2025/src/com/mg/lab2/Interest.java
+++ COMP228_Lab2_MG_F2025/src/com/mg/lab2/Interest.java
@@ -1,5 +1,99 @@
 package com.mg.lab2;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 public class Interest {
 
+	private double principal;
+	private double rate;
+	private int time;
+	private int n;
+	
+	public Interest(double principalData, double rateData, int timeData) {
+		if(Double.isNaN(principalData) || Double.isNaN(rateData) || Double.isNaN(timeData) ) {
+			System.out.print("Please enter only Intiger values");
+		}else if ((principalData < -1) || (rateData < -1) || (timeData < -1)) {
+			System.out.print("Please enter only Positive values");
+		}			
+		
+		this.principal = (double) principalData;
+		this.rate = (double) rateData;
+		this.time = (int) timeData;
+		
+		System.out.println("Principal: "+principal);
+		System.out.println("Rate: "+rate);
+		System.out.println("Time: "+time);
+				
+		double SI = SimpleInterest(principal,rate,time);
+		System.out.printf("Simple Interest Rate Double: %.2f%n",SI);		
+		
+		BigDecimal SIbg = SimpleInterestBigDec(principal,rate,time);
+		System.out.printf("Simple Interest Rate BigDecimal: %n"+SIbg);		
+	}
+	
+	public Interest(double principalData, double rateData, int timeData, int nData) {
+
+		if(Double.isNaN(principalData) || Double.isNaN(rateData) || Double.isNaN(timeData) || Double.isNaN(nData) ) {
+			System.out.print("Please enter only Intiger values");
+		}else if ((principalData < -1) || (rateData < -1) || (timeData < -1) || (nData < -1)) {
+			System.out.print("Please enter only Positive values");
+		}			
+		
+		this.principal = (double) principalData;
+		this.rate = (double) rateData;
+		this.time = (int) timeData;
+		this.n = (int) nData;
+		
+		System.out.println("\nCompounding periods/yr: "+n);
+		
+		double CI = CompoundInterest(principal,rate,time,n);
+		System.out.printf("Compound Interest Rate Double: %.2f",CI);
+		
+		BigDecimal CIbg = CompoundInterestBigDec(principal,rate,time,n);
+		System.out.println("Compound Interest Rate Big Decimal: "+CIbg);
+	}
+	
+	// method for calculating SI = PxRxT/100;
+	
+	double SimpleInterest(double principal, double rate, int time) {
+		double result = (principal*rate*time)/100;
+		return result;
+	}
+	
+	// method for calculating COMP INT
+	double CompoundInterest(double principal, double rate, int time, int n) {
+		double rDecimal = rate/100.0;
+		double amount = principal*Math.pow(1+rDecimal / n,n*time);
+		return amount - principal;
+	}
+	
+	BigDecimal SimpleInterestBigDec(double principal, double rate, int time) {
+		BigDecimal step1 = BigDecimal.valueOf(principal).multiply(BigDecimal.valueOf(rate));
+		BigDecimal step2 = (step1.multiply(BigDecimal.valueOf(time)));
+		BigDecimal result = step2.divide(BigDecimal.valueOf(100),10, RoundingMode.HALF_UP);
+		return result;
+	}
+	
+	// method for calculating COMP INT
+	BigDecimal CompoundInterestBigDec(double principal, double rate, int time, int n) {
+		BigDecimal P = BigDecimal.valueOf(principal);
+        BigDecimal r = BigDecimal.valueOf(rate).divide(BigDecimal.valueOf(100), 10, RoundingMode.HALF_UP);
+        BigDecimal one = BigDecimal.ONE;
+
+        // (1 + r/n)
+        BigDecimal base = one.add(r.divide(BigDecimal.valueOf(n), 10, RoundingMode.HALF_UP));
+
+        // exponent = n * t
+        int exponent = n * time;
+
+        // (1 + r/n)^(n*t)
+        BigDecimal factor = base.pow(exponent);
+
+        // A = P * factor
+        BigDecimal amount = P.multiply(factor);
+
+        // CI = A - P
+        return amount.subtract(P).setScale(10, RoundingMode.HALF_UP);
+	}
 }
+
